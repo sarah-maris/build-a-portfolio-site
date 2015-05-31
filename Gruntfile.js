722b6a9aa443b4c20df135136f0e75d8e3e89b8c
root <root@ip-10-47-174-141.ec2.internal>
2015-05-31 16:18:21 +0000

    Committed by Udacity

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{            
+            width: 2000,
+            suffix: '_large_2x',
+            quality: 30       
+          }, {            
+            width: 1000,
+            suffix: '_large_1x',
+            quality: 30       
+          }, {          
+            width: 1200,
+            suffix: '_med_2x',
+            quality: 30       
+          },{          
+            width: 600,
+            suffix: '_med_1x',
+            quality: 30       
+          },{          
+            width: 600,
+            suffix: '_sm_2x',
+            quality: 30       
+          },{          
+            width: 300,
+            suffix: '_sm_1x',
+            quality: 30       
+          },{                       
+            width: 800,
+            suffix: '_res_2x',
+            quality: 30       
+          },{            
+            width: 400,
+            suffix: '_res_1x',
+            quality: 30       
+          }, 
+		  ]
+        },
+
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: 'images_src/logo/*.{gif,jpg,png,.svg}',
+          dest: 'images/'
+        }]
+      },
+    },
+  });
+  
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
